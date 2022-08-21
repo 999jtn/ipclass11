f4901fe33f48e6db8e76cdc0b1a9809d53bd1eba
Jatin <101134139+clownforlyf@users.noreply.github.com>
2022-08-21 10:52:28 +0530

    Create Program - 4.2.py

---
+++ Working with Lists & Dictionaries/Program - 4.2.py
@@ -0,0 +1,9 @@
+# A simple python program to find the soln of questions in list.
+
+list1 = [20,30,40,50,60,70,80] #list to test questions
+
+print()
+print("Average of values in the list: " + str(sum(list1)/len(list1)))
+print("Maximum value in the list: " + str(max(list1)))
+print("Minimum value in the list: " + str(min(list1)))
+print("Sum of values in the list: " + str(sum(list1)))
